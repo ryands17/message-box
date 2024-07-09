b7b376afc4f5f3854add1385642fc93046a709c1
Ryan Dsouza <r.dsouza@newmotion.com>
2024-07-09 15:58:09 +0200

    astro actions with htmx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid', // or 'server'
+  output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,7 @@
+import { experimental_AstroContainer } from 'astro/container';
 import { defineAction, ActionError, z } from 'astro:actions';
 import { db, message } from 'db/schema';
+import MessageDisplay from 'components/MessageDisplay.astro';
 
 const sleep = (time = 1000) =>
   new Promise((resolve) => setTimeout(() => resolve(true), time));
@@ -13,12 +15,22 @@ export const server = {
     }),
     handler: async ({ content, userId }) => {
       try {
-        await sleep(2000);
-        await db.insert(message).values({
-          content,
-          createdBy: userId,
+        await sleep();
+        const data = await db
+          .insert(message)
+          .values({
+            content,
+            createdBy: userId,
+          })
+          .returning();
+
+        console.log({ data });
+        const container = await experimental_AstroContainer.create();
+        const result = await container.renderToString(MessageDisplay, {
+          props: { message: data[0], userId },
         });
-        return true;
+
+        return result;
       } catch (error) {
         throw new ActionError({
           message: `Couldn't send the message. Please try again.`,
